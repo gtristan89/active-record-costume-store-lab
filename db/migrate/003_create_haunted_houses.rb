abdbffdb4932e3775bccf542839c1bf0d899abb1
Tristan Gibbs <gtristan89@gmail.com>
2021-04-27 20:50:26 -0700

    commit tree c177811be2cb7797612f215cf23d9a078c3755aa

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
             t.string :location
             t.string :theme
             t.string :price
-            t.string :family_friendly
+            t.boolean :family_friendly
             t.string :opening_date
             t.string :closing_date
             t.string :description
